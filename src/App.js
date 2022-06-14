42a24f505c17ffbc18f9d0d9be056c960cde577f
Bartosz Lewandowski <89294735+Bart3kL@users.noreply.github.com>
2022-06-14 18:28:24 +0200

    Navigation - appearance update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Layout from './components/Layout/Layout';
 import './App.css';
 
 function App() {
-  return <Layout>Lorem*100</Layout>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="edudor" />} />
+        <Route path="edudor" />
+        <Route path="nauka" />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
--- src/components/Layout/Navigation.js
+++ src/components/Layout/Navigation.js
@@ -1,20 +1,50 @@
+import { NavLink, Link } from 'react-router-dom';
+import { IoIosArrowDown } from 'react-icons/io';
+
 import styles from './Navigation.module.css';
 
 const Navigation = () => {
+  const activePage = (data) =>
+    data.isActive ? `${styles.activePageUnderline}` : '';
+
   return (
     <header className={styles.header}>
-      <h2 className={styles.headerLogo}>Edudor</h2>
+      <Link className={styles.headerLogo} to="/edudor">
+        Edudor
+      </Link>
       <nav className={styles.navigation}>
         <ul>
-          <li>Nauka</li>
-          <li>Słownik</li>
-          <li>Gramatyka</li>
-          <li>Zwroty</li>
-          <li>Kontakt</li>
+          <li>
+            <NavLink to="/nauka" className={activePage}>
+              Nauka
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/slownik" className={activePage}>
+              Słownik
+              <IoIosArrowDown className={styles.navArrow} />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/gramatyka" className={activePage}>
+              Gramatyka
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/zwroty" className={activePage}>
+              Zwroty
+              <IoIosArrowDown className={styles.navArrow} />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/kontakt" className={activePage}>
+              Kontakt
+            </NavLink>
+          </li>
         </ul>
       </nav>
       <div className={styles.authBtns}>
-        <button>Zaloguj się</button>
+        <button className={styles.authBtn}>Zaloguj się</button>
       </div>
     </header>
   );
