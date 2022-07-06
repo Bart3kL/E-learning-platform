13fd2e3878507d6613dc17d3d3f4d28fbd3c4c0f
Bartosz Lewandowski <89294735+Bart3kL@users.noreply.github.com>
2022-07-06 22:08:46 +0200

    FlashCard/RepeatWords components - added

--- src/components/Science/Flashcards.js
+++ src/components/Science/Flashcards.js
@@ -1,11 +1,61 @@
+import { BsArrowLeftCircle, BsArrowRightCircle } from 'react-icons/bs';
+
 import ScienceNav from './ScienceNav';
-import styles from './Flashcards.module.css'
+import styles from './Flashcards.module.css';
+import { useState } from 'react';
 
 const Flashcards = () => {
-    return (     <div className="science">
-    <ScienceNav />
-    <section className="sciencePages">flashcards</section>
-  </div> );
-}
- 
+  const [showWord, setShowWord] = useState(false);
+  const showWordHandler = () => {
+    setShowWord(!showWord);
+  };
+  return (
+    <div className="science">
+      <ScienceNav />
+      <section className="scienceContainer">
+        <div className={styles.flashcardsHeader}>
+          <h2 className={styles.flashcardsHeaderName}>Twoje fiszki</h2>
+          <p className={styles.flashcardsHeaderText}>
+            Fiszki pochodzą z twojego słownika, możesz je także dodać klikając +
+            obok słowa.
+          </p>
+        </div>
+        <div className={styles.flashcards}>
+          {showWord ? (
+            <div className={styles.flascard} onClick={showWordHandler}>
+              <h3 className={styles.flashcardHint}>
+                Aby zobaczy słowo kliknij w okno.
+              </h3>
+              <div className={styles.flashcardWord}>Skirt</div>
+              <div className={styles.flashcardChangeWord}>
+                <div className={styles.flashcardChange}>
+                  <BsArrowLeftCircle />
+                </div>
+                <div className={styles.flashcardChange}>
+                  <BsArrowRightCircle />
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className={styles.flascard} onClick={showWordHandler}>
+              <h3 className={styles.flashcardHint}>
+                Aby zobaczy tłumaczenie kliknij w okno.
+              </h3>
+              <div className={styles.flashcardWord}>Sukienka</div>
+              <div className={styles.flashcardChangeWord}>
+                <div className={styles.flashcardChange}>
+                  <BsArrowLeftCircle />
+                </div>
+                <div className={styles.flashcardChange}>
+                  <BsArrowRightCircle />
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </section>
+    </div>
+  );
+};
+
 export default Flashcards;
--- src/components/Science/RepeatWords.js
+++ src/components/Science/RepeatWords.js
@@ -1,8 +1,13 @@
 import ScienceNav from './ScienceNav';
 
 import styles from './RepeatWords.module.css';
+import { useState } from 'react';
 
 const RepeatWords = () => {
+  const [checkAnswer, setCheckAnswer] = useState(false);
+  const checkAnswerHandler = () => {
+    setCheckAnswer(true);
+  };
   return (
     <div className="science">
       <ScienceNav />
@@ -12,10 +17,43 @@ const RepeatWords = () => {
           <div className={styles.repeatWordName}>
             <span className={styles.word}>siostra</span>
           </div>
-          <textarea
-            className={styles.repeatWordName}
-            placeholder="tłumaczenie.."
-          ></textarea>
+
+          {checkAnswer ? (
+            <div
+              className={styles.repeatWordName}
+              placeholder="tłumaczenie..."
+            ></div>
+          ) : (
+            <textarea
+              className={styles.repeatWordName}
+              placeholder="tłumaczenie..."
+            ></textarea>
+          )}
+          {checkAnswer ? (
+            <div className={styles.check}>
+              <button className={styles.qualityBtn1 + ' ' + styles.qualityBtn}>
+                Źle
+              </button>
+              <p className={styles.fastClick}>klawisz 1</p>
+              <button className={styles.qualityBtn2 + ' ' + styles.qualityBtn}>
+                Średnio
+              </button>
+              <p className={styles.fastClick}>klawisz 2</p>
+              <button className={styles.qualityBtn3 + ' ' + styles.qualityBtn}>
+                Dobrze
+              </button>
+              <p className={styles.fastClick}>klawisz 3</p>
+            </div>
+          ) : (
+            <div className={styles.check}>
+              <button className={styles.checkBtn}>Podpowiedz literę</button>
+              <p className={styles.fastClick}>klawisz space</p>
+              <button className={styles.checkBtn} onClick={checkAnswerHandler}>
+                Sprawdź odpowiedź
+              </button>
+              <p className={styles.fastClick}>klawisz enter</p>
+            </div>
+          )}
         </div>
       </section>
     </div>
